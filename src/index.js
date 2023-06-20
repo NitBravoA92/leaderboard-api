19873d036b3f8eeb8c3a125222cda17992618a48
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-20 15:46:09 -0400

    Fix some sintax, spacing and indentation issues

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './style.css';
 import listRecentScores from './modules/scores.js';
+
 listRecentScores();
--- src/modules/scores.js
+++ src/modules/scores.js
@@ -1,34 +1,34 @@
 const generateScoreMarkup = ({ user, score }) => `<li>${user}: ${score}</li>`;
 const insertScoreInDOM = (scores) => {
-  const allLiScores = scores.map(score => generateScoreMarkup(score));
+  const allLiScores = scores.map((score) => generateScoreMarkup(score));
   document.querySelector('#scores-list').innerHTML = allLiScores.join('');
-}
+};
 const listRecentScores = () => {
   const scores = [
     {
-      user: "Louis Parker",
+      user: 'Louis Parker',
       score: 59,
     },
     {
-      user: "Robert Mendez",
+      user: 'Robert Mendez',
       score: 20,
     },
     {
-      user: "Carmen Bravo",
+      user: 'Carmen Bravo',
       score: 75,
     },
     {
-      user: "Patrick Linderman",
+      user: 'Patrick Linderman',
       score: 90,
     },
     {
-      user: "Annie Smith",
+      user: 'Annie Smith',
       score: 43,
     },
     {
-      user: "James Williams",
+      user: 'James Williams',
       score: 81,
-    }
+    },
   ];
   insertScoreInDOM(scores);
 };
