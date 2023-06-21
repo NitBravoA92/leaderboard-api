60d9aaec4ecd689a6ac4d70e649c88ff0c2f69e9
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-21 14:28:48 -0400

    Refactor functions in scores.js module

--- src/modules/scores.js
+++ src/modules/scores.js
@@ -1,36 +1,10 @@
-const generateScoreMarkup = ({ user, score }) => `<li>${user}: ${score}</li>`;
-const insertScoreInDOM = (scores) => {
-  const allLiScores = scores.map((score) => generateScoreMarkup(score));
-  document.querySelector('#scores-list').innerHTML = allLiScores.join('');
+const insertScoreInDOM = ({ user, score }) => {
+  const liElement = document.createElement('li');
+  liElement.textContent = `${user}: ${score}`;
+  document.querySelector('#scores-list').appendChild(liElement);
 };
-const listRecentScores = () => {
-  const scores = [
-    {
-      user: 'Louis Parker',
-      score: 59,
-    },
-    {
-      user: 'Robert Mendez',
-      score: 20,
-    },
-    {
-      user: 'Carmen Bravo',
-      score: 75,
-    },
-    {
-      user: 'Patrick Linderman',
-      score: 90,
-    },
-    {
-      user: 'Annie Smith',
-      score: 43,
-    },
-    {
-      user: 'James Williams',
-      score: 81,
-    },
-  ];
-  insertScoreInDOM(scores);
+const listRecentScores = (scores) => {
+  scores.forEach((score) => insertScoreInDOM(score));
 };
 
 export default listRecentScores;
