ee8cbd6dc28d47a2b38c779b77adeb6805146c2a
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-21 22:34:55 -0400

    Create the functionality of the refresh button

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ import saveNewScore from './modules/saveScores';
 loadScores();
 
 const formSaveScore = document.querySelector('#add-score-form');
+const btnRefresh = document.querySelector('#btn-refresh');
+
 formSaveScore.addEventListener('submit', (event) => {
   event.preventDefault();
   const gameId = localStorage.getItem('game-id');
   const { name, score } = formSaveScore;
   saveNewScore(name.value, Number(score.value), gameId);
   formSaveScore.reset();
+});
+
+btnRefresh.addEventListener('click', () => {
+  document.querySelector('#refreshing-message').innerHTML = 'Data scores are reloaded!';
+  setTimeout(() => {
+    document.querySelector('#refreshing-message').innerHTML = '';
+  }, 2000);
+  loadScores();
 });
 
 
--- src/modules/saveScores.js
+++ src/modules/saveScores.js
@@ -17,10 +17,10 @@ const saveNewScore = async (user, score, gameId) => {
       'Content-Type': 'application/json; charset=UTF-8'
     }
   };
-    const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`, options);
-    const finalResult = await response.json();
-    insertScoreInDOM(data);
-    displaySuccessMessage(finalResult.result);
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`, options);
+  const finalResult = await response.json();
+  insertScoreInDOM(data);
+  displaySuccessMessage(finalResult.result);
 }
 
 export default saveNewScore;
