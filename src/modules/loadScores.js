d94f3b2b1bdb7dc76f4e5c21201e805255bd12e3
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-23 00:58:17 -0400

    Fix some indentation, sintax and spacing issues

--- src/modules/loadScores.js
+++ src/modules/loadScores.js
@@ -3,7 +3,7 @@ import getScores from './getScores.js';
 import { listRecentScores } from './showScores.js';
 
 const loadScores = async () => {
-  let gameId = localStorage.getItem('game-id') || (await newGameId());
+  const gameId = localStorage.getItem('game-id') || (await newGameId());
   const allScores = await getScores(gameId);
   listRecentScores(allScores);
 };
